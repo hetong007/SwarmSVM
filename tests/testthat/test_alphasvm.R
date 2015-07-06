b489dab2e337324b1dade984b8c8f9ab6bc1ef4f
hetong007 <hetong007@gmail.com>
2015-07-05 18:31:35 -0700

    add simple tests for alphasvm

---
+++ tests/testthat/test_alphasvm.R
@@ -0,0 +1,34 @@
+require(SwarmSVM)
+
+context("alphasvm")
+
+data(svmguide1)
+svmguide1.t = svmguide1[[2]]
+svmguide1 = svmguide1[[1]]
+
+data(iris)
+
+test_that("Error Trigger",{
+  expect_error({ model = alphasvm(x = iris[,-5], y = iris[,5], alpha = 0) })
+  expect_error({ model = alphasvm(x = iris[,-5], y = iris[,5], alpha = rep(0,nrow(iris))) })
+  expect_error({ model = alphasvm(x = svmguide1[,-1], y = svmguide1[1:100,1])})
+})
+
+test_that("Performance",{
+  set.seed(1024)
+  model = alphasvm(x = svmguide1[,-1], y = svmguide1[,1], cost = 32)
+  preds = predict(model, svmguide1.t[,-1])
+  score = sum(diag(table(preds,svmguide1.t[,1])))/nrow(svmguide1.t)
+  expect_true(score>0.8)
+  
+  new.alpha = matrix(0, nrow(svmguide1),1)
+  new.alpha[model$index,] = model$coefs
+  model2 = alphasvm(x = svmguide1[,-1], y = svmguide1[,1], alpha = new.alpha, cost = 32)
+  preds = predict(model2, svmguide1.t[,-1])
+  score = sum(diag(table(preds,svmguide1.t[,1])))/nrow(svmguide1.t)
+  expect_true(score>0.8)
+  # compare the alpha changes:
+  # plot(model$coefs[match(ind,model$index),],model2$coefs[match(ind,model2$index),],pch=20)
+})
+
+
--- tests/testthat/test_dcSVM.R
+++ tests/testthat/test_dcSVM.R
@@ -1,7 +1,13 @@
 require(SwarmSVM)
 
-context("Error Trigger for dcSVM")
+context("dcSVM")
 
 data(svmguide1)
 svmguide1.t = svmguide1[[2]]
 svmguide1 = svmguide1[[1]]
+
+data(iris)
+
+test_that("Error Trigger",{
+  
+})
